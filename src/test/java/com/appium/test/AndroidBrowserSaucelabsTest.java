e99aaf023761c652b378aeb8e32f67aa95bbd7fa
padmakarsrinivas <srinivas_padmakar@yahoo.com>
2018-08-06 19:35:18 +0530

    Changed capabilities for Sauce Labs tests.

--- src/test/java/com/appium/test/AndroidBrowserSaucelabsTest.java
+++ src/test/java/com/appium/test/AndroidBrowserSaucelabsTest.java
@@ -17,9 +17,9 @@ import io.appium.java_client.remote.MobileCapabilityType;
 /**
  * Android Browser Sauce Labs Test.
  */
-public class AndroidBrowserSaucelabsTest extends BaseTest {
-  public static final String USERNAME = "YOUR_USERNAME";
-  public static final String ACCESS_KEY = "YOUR_ACESS_KEY";
+public class AndroidBrowserSaucelabsTest extends BaseTest{
+  public static final String USERNAME = "padmakarsrinivas";
+  public static final String ACCESS_KEY = "341f906a-fb12-421b-a6dd-8bf97221d429";
   public static final String URL = "https://"+USERNAME+":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
   public static AndroidDriver<?> mobiledriver;
 
@@ -27,11 +27,12 @@ public class AndroidBrowserSaucelabsTest extends BaseTest {
   public void beforeTest( ) throws MalformedURLException 
   {
     DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.4");
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1");
     capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
     capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Samsung Galaxy S4 Emulator");
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Samsung Galaxy S8 WQHD GoogleAPI Emulator");
     capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+    capabilities.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.8.1");
     capabilities.setCapability("newCommandTimeout", 2000);
     mobiledriver = new AndroidDriver<>(new URL(URL), capabilities);
   }
--- src/test/java/com/appium/test/IOSBrowserSaucelabsTest.java
+++ src/test/java/com/appium/test/IOSBrowserSaucelabsTest.java
@@ -18,8 +18,8 @@ import io.appium.java_client.remote.MobileCapabilityType;
  * IOS Browser Sauce Labs Test.
  */
 public class IOSBrowserSaucelabsTest extends BaseTest{
-  public static final String USERNAME = "YOUR_USERNAME";
-  public static final String ACCESS_KEY = "YOUR_ACESS_KEY";
+  public static final String USERNAME = "padmakarsrinivas";
+  public static final String ACCESS_KEY = "341f906a-fb12-421b-a6dd-8bf97221d429";
   public static final String URL = "https://"+USERNAME+":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
   public static IOSDriver<?> mobiledriver;
 
@@ -27,11 +27,12 @@ public class IOSBrowserSaucelabsTest extends BaseTest{
   public void beforeTest( ) throws MalformedURLException 
   {
     DesiredCapabilities capabilities = new DesiredCapabilities();
-    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.4");
+    capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11.3");
     capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"iOS");
     capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"XCUITest");
-    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone Simulator");
+    capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 8 Simulator");
     capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Safari");
+    capabilities.setCapability(MobileCapabilityType.APPIUM_VERSION, "1.8.1");
     capabilities.setCapability("newCommandTimeout", 2000);
     mobiledriver = new IOSDriver<>(new URL(URL), capabilities);
 
